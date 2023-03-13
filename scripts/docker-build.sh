9ca84e6bc347a53812a27336e77d8959501f5fcf
luis-falva <luis.vargas@lumiata.com>
2023-03-13 02:17:05 -0600

    add: make test-all instructions to README.md

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,8 +5,8 @@ image="titanic_kernel_svm_mlops"
 tag=${1:-"latest"}
 
 # Build the Docker image
-echo docker build -t "$image":"$tag" .
+docker build -t "$image":"$tag" .
 
 # Tag the image with the registry name
 registry_image_name="luisfalva/$image"
-echo docker tag "$image":"$tag" "$registry_image_name":"$tag"
+docker tag "$image":"$tag" "$registry_image_name":"$tag"
